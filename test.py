ab84f7909f1961d479c4006ebb13a9467a992281
Ruhi1010 <rashedial52@gmail.com>
2025-03-01 10:21:29 +0600

    .

---
+++ test.py
@@ -0,0 +1,23 @@
+#3.1
+names = ["Ruhi", "Amit", "Rakib", "Plabon"]
+names.sort()
+for name in names:
+    print(name)
+
+#3.2
+for name in names:
+    print(f"Hello, {name}! How are you today?")
+
+#3.3
+transportation = ["Honda motorcycle", "Tesla Model S", "bicycle"]
+for item in transportation:
+    if "motorcycle" in item:
+        print(f"I would like to own a {item}.")
+    elif "Tesla" in item:
+        print(f"I would like to own a {item}.")
+    elif "bicycle" in item:
+        print(f"I would like to own a {item}.")
+    elif "walking" in item:
+        print(f"I would like to own a {item}.")
+    else:
+        print(f"I would like to own a {item}.")
