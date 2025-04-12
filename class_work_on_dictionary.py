12235f2637034464cc5cd31c30ec6cf262ee3c2f
Ruhi1010 <rashedial52@gmail.com>
2025-04-12 23:07:06 +0600

    .

---
+++ class_work_on_dictionary.py
@@ -0,0 +1,45 @@
+# 6-4
+glossary = {
+     'dictionary': 'A collection of key-value pairs.',
+     'list': 'An ordered collection of items.',
+     'tuple': 'An immutable list.'
+}
+for key,val in glossary.items():
+    print(f"key:{key}")
+    print(f"value:{val}") 
+print()
+print()
+
+# 6-5
+rivers = {
+    'nile': 'egypt',
+    'amazon': 'brazil',
+    'padma': 'bangladesh'
+}
+
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country.title()}.")
+
+for river in rivers.keys():
+    print(f"{river.title()}")
+
+print()
+
+for country in rivers.values():
+    print(f"{country.title()}")
+
+
+# 6-6
+favorite_languages = {
+'jen': 'python',
+'sarah': 'c',
+'edward': 'ruby',
+'phil': 'python',
+}
+
+people = ['jen', 'sarah', 'ruhi', 'rishat', 'edward', 'phil', 'rakib']
+for person in people:
+    if person in favorite_languages:
+        print(f"Thank you {person.title()}, for the pole")
+    else:
+        print(f"Not Thanking you {person.title()}")
---
+++ personal_practice.py
@@ -0,0 +1,88 @@
+# MAX = 100
+# MAX = 200
+# print(MAX) 
+
+# favorite = 100
+# print(f"my favorite number is {favorite}")
+
+# import this 
+
+
+lists = ['ruhi', 'pujon', 'plabon', 'rakib']
+# print(lists[0].title())
+# print(lists[-2])
+# massage = f"my son name is {lists[-2].title()}"
+# print(massage)
+
+# lists.append('ayon')
+# print(lists)
+
+# lists.insert(5, 'sumon')
+# lists.insert(6, 'rashedi')
+# print(lists)
+# del lists[-1]
+# print(lists)
+
+# popped_list = lists.pop()
+# print(popped_list.title())
+# print(lists)
+# # lists.sort()
+# # print(lists)
+# print(sorted(lists))
+# print(len(lists))
+# arr = [1,2,3,4,5]
+# print(arr)
+# arr.reverse()
+# print(arr) 
+# for array in arr:
+#     print(f"{array}")
+# print("\n")
+# for num in range(1, 20):
+#     print(num)
+# nums = list(range(21, 30))
+# print(nums)
+
+# squares = []
+# for value in range(1, 11):
+#     # square = value**2
+#     squares.append(value**2)
+# print(squares)
+# print(min(squares))
+# print(max(squares))
+# print(sum(squares))
+
+# print(squares[-2:])
+
+# print("Here is the list of 1 to 5 squares: ")
+# for square in squares[:5]:
+#     print(square, end=" ")
+
+# square_of_ten_numbers = squares[:]
+# print("Here is the original list: ")
+# print(squares)
+# print("Here is the copied list")
+# square_of_ten_numbers.append(200)
+# print(square_of_ten_numbers)
+
+
+# dimension = (20, 200)
+# print(dimension[0])
+# print(dimension[1])
+# print()
+# dimension = (40, 400)
+# for dimensions in dimension:
+#     print(dimensions, end=" ") 
+
+
+# arr = [7]
+# print(arr[0].bit_count())
+
+# dict = {
+#     'name': 'Rashedi',
+#     'age': 25,
+#     'city': 'Dhaka'
+# }
+
+# dict['area'] = 'Mayakanon'
+
+# print (dict)
